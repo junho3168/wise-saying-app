d97c4425007e6efaf0569471d377dfe4cf0915b4
junho3168 <junho3168@naver.com>
2024-11-26 22:32:04 +0900

    등록work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,45 +1,8 @@
 package com.ll;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         App app = new App();
         app.run();
-
     }
-}
-
-class App {
-    public void run() {
-
-        System.out.println("== 명언 앱 ==");
-
-        Scanner scanner = new Scanner(System.in);
-
-        int lastId = 0;
-
-        while (true) {
-            System.out.println("명령) ");
-
-
-            String cmd = scanner.nextLine();
-
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("등록")) {
-                System.out.print("명언: ");
-                String content = scanner.nextLine();
-                System.out.println("작가: ");
-                String author = scanner.nextLine();
-                int id = ++lastId;
-                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
-
-            }
-
-
-        }
-        scanner.close();
-    }
-
 }
